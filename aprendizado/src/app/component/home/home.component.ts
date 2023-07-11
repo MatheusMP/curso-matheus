eebe5a325fe832238cedc05a0ca2385d3ade4a61
Matheus Pinheiro <126804529+Matheus-M-Pinheiro@users.noreply.github.com>
2023-07-10 21:37:55 -0300

    CRUD

--- aprendizado/src/app/component/home/home.component.ts
+++ aprendizado/src/app/component/home/home.component.ts
@@ -14,14 +14,16 @@ export class HomeComponent implements OnInit {
   produtos: Produto[] = [];
   modalAberto: boolean = false;
   formularioProduto: FormGroup;
+  edicaoProduto: boolean = false;
+  idDaEdicao: number = 0;
 
   constructor(
     private apiProduto: ProdutosService
   ) {
     this.formularioProduto = new FormGroup({
       nome: new FormControl('', [Validators.required, Validators.minLength(10)]),
-      preco: new FormControl('', [Validators.required]),
-      estoque: new FormControl('', [Validators.required])
+      preco: new FormControl('', [Validators.required, Validators.min(100)]),
+      estoque: new FormControl('', [Validators.required, Validators.min(1000)])
     })
   }
 
@@ -29,23 +31,60 @@ export class HomeComponent implements OnInit {
     this.pegaProdutos()
   }
 
-  abreModal(): void{
+  abreModal(): void {
+    this.formularioProduto.controls['nome'].setValue('')
+    this.formularioProduto.controls['preco'].setValue('')
+    this.formularioProduto.controls['estoque'].setValue('')
+    this.edicaoProduto = false
     this.modalAberto = true
   }
-  fechaModal(): void{
+
+  fechaModal(): void {
     this.modalAberto = false
   }
 
-  pegaProdutos(): void{
-    this.apiProduto.getTodosProdutos().subscribe( (respApi) => {
+  abrirModalEditar(idDoProduto: number): void {
+    this.apiProduto.getProdutoPeloId(idDoProduto).subscribe((respApi) => {
+      this.formularioProduto.controls['nome'].setValue(respApi.nomeProduto)
+      this.formularioProduto.controls['preco'].setValue(respApi.precoProduto)
+      this.formularioProduto.controls['estoque'].setValue(respApi.estoqueProduto)
+      this.idDaEdicao = idDoProduto
+      this.edicaoProduto = true
+      this.modalAberto = true
+    })
+  }
+
+  pegaProdutos(): void {
+    this.apiProduto.getTodosProdutos().subscribe((respApi) => {
       this.produtos = respApi
     })
   }
 
-  deletarProduto( id: number ): void{
-    this.apiProduto.deleteProduto( id ).subscribe( (respApi) => {
+  deletarProduto(id: number): void {
+    this.apiProduto.deleteProduto(id).subscribe((respApi) => {
       this.pegaProdutos()
     })
+  }
+
+  finalizarFormulario(): void {
+    // Não precisa de validação de valores dos campos por causa do FormGroup
+    let produtoParaEnviar: Produto = new Produto
+    produtoParaEnviar.nomeProduto = this.formularioProduto.value.nome
+    produtoParaEnviar.precoProduto = this.formularioProduto.value.preco
+    produtoParaEnviar.estoqueProduto = this.formularioProduto.value.estoque
+    if( this.edicaoProduto == true ){
+      // Vai entrar aqui quando for a edição de um produto
+      produtoParaEnviar.id = this.idDaEdicao
+      this.apiProduto.putEditaProduto(produtoParaEnviar).subscribe( (respApi) => {
+        this.pegaProdutos()
+        this.modalAberto = false
+      })
+    } else {
+      this.apiProduto.postCriarNovoProduto(produtoParaEnviar).subscribe((respApi) => {
+        this.pegaProdutos()
+        this.modalAberto = false
+      })
+    }
   }
 
 }
--- aprendizado/src/app/service/produtos.service.ts
+++ aprendizado/src/app/service/produtos.service.ts
@@ -18,8 +18,21 @@ export class ProdutosService {
     return this.http.get<Produto[]>(`${this.urlDaApi}/produtos`) // http://localhost:3000/produtos
   }
 
+  getProdutoPeloId( idDoProduto: number ): Observable<Produto>{
+    return this.http.get<Produto>(`${this.urlDaApi}/produtos/${idDoProduto}`) // http://localhost:3000/produtos/ID-DO-PRODUTO
+  }
+
   deleteProduto( idDoProduto: number ): Observable<Produto>{
     return this.http.delete<Produto>(`${this.urlDaApi}/produtos/${idDoProduto}`) // http://localhost:3000/produtos/ID-DO-PRODUTO
+  }
+
+  postCriarNovoProduto( produtoNovo: Produto ): Observable<Produto>{
+    return this.http.post<Produto>(`${this.urlDaApi}/produtos`, produtoNovo) // Passamos URL e o objeto do novo produto
+  }
+
+  putEditaProduto( produtoAlterado: Produto ): Observable<Produto>{
+    // Precisa do Id do produto e o objeto do produto alterado
+    return this.http.put<Produto>(`${this.urlDaApi}/produtos/${produtoAlterado.id}`, produtoAlterado)
   }
 
 }
