8e86e5d5516abd4cfc8c20e0f61c74f5ab7fe652
Matheus Pinheiro <126804529+Matheus-M-Pinheiro@users.noreply.github.com>
2024-01-15 21:06:38 -0300

    Swal

--- projeto-padaria/src/app/component/comandas/comandas.component.ts
+++ projeto-padaria/src/app/component/comandas/comandas.component.ts
@@ -4,6 +4,7 @@ import { ComandaAberta } from 'src/app/model/comanda-aberta';
 import { Produto } from 'src/app/model/produto';
 import { GeralService } from 'src/app/service/geral.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-comandas',
@@ -47,9 +48,22 @@ export class ComandasComponent implements OnInit {
     })
   }
 
+  // Função para criar uma comanda
   criarComanda(): void{
-    this.api.postCriarComanda(new ComandaAberta()).subscribe((resp) => {
-      this.getComandas()
+    // Acionando um SWAL para o usuário confirmar se quer abrir uma comanda
+    Swal.fire({
+      title: 'Criar uma nova comanda?', // Define o texto principal
+      icon: 'question', // Define o ícone do SWAL
+      showDenyButton: true, // Define se o botão de negar sera mostrado
+      confirmButtonText: 'Sim!', // Define o texto do botão de confirmar
+      denyButtonText: 'Não!' // Define o texto do botão de negar
+    }).then((resultado) => { // Uma função para quando uma ação dentro do SWAL for tomada
+      console.log(resultado)
+      if(resultado.isConfirmed){ // Caso o usuário confirme a criação
+        this.api.postCriarComanda(new ComandaAberta()).subscribe((resp) => {
+          this.getComandas()
+        })
+      }
     })
   }
 
@@ -57,12 +71,14 @@ export class ComandasComponent implements OnInit {
     this.modalAberto = false
   }
 
+  // Abre o modal de Adicionar produto em uma comanda que foi clicada
   abriModalAdicionar(idComandaClicada: number): void{
     this.idComandaSelecionada = idComandaClicada
     this.modalAberto = true
     this.modalAdicionar = true
   }
 
+  // Adiciona um produto escolhido da comanda que esta aberta
   adicionaProduto(): void{
     this.api.getComandaPorId(this.idComandaSelecionada).subscribe((comandaResp) => {
       let comandaModificada = comandaResp
@@ -88,6 +104,10 @@ export class ComandasComponent implements OnInit {
       }
       
       this.api.putEditaComanda(comandaModificada).subscribe((resp) => {
+        Swal.fire({
+          title: 'Produto adicionado',
+          icon: 'success'
+        })
         this.getComandas()
         this.modalAberto = false
       })
